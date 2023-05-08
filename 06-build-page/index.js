e81af57ca42e4d4ac641a4317c1d4fea40deb34f
eskovdmt <eskovdmt@gmail.com>
2023-05-09 01:13:09 +0300

    feat: correct solution to 06-build-page after read terns of reference

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -4,21 +4,18 @@ const {readdir, rm} = require("fs/promises")
 const {copyFile} = require("fs/promises")
 const fs = require("fs");
 const {stat} = require("fs/promises")
-const {write} = require("fs");
 
 const directoryAssets = path.join(__dirname, "assets")
 const directoryProjectDist = path.join(__dirname, "project-dist")
 const directoryStyles = path.join(__dirname, "styles")
-const cssBundle = path.join(__dirname, "project-dist", "bundle.css")
+const cssBundle = path.join(__dirname, "project-dist", "style.css")
+const assetsDirectory = path.join(__dirname, "project-dist", "assets")
 const templateHtml = path.join(__dirname, "template.html")
 const copyTemplateHtml = path.join(__dirname, "project-dist", "template.html")
-const articlesPath = path.join(__dirname, "components", "articles.html")
-const footerPath = path.join(__dirname, "components", "footer.html")
-const headerPath = path.join(__dirname, "components", "header.html")
-let htmlData = []
-let header = writeComponents(headerPath)
-let articles = writeComponents(articlesPath)
-let footer = writeComponents(footerPath)
+const componentsDirectory = path.join(__dirname, "components")
+
+let components = {}
+let tags = []
 
 async function removeDirectory(directory) {
   await rm(directory, {recursive: true});
@@ -28,84 +25,82 @@ async function createDirectory(directory) {
   await mkdir(directory, {recursive: true});
 };
 
-async function getFiles(directory, flag = "directory") {
-  if (flag === "directory") {
-    return await readdir(directory, {withFileTypes: true})
-  }
-  if (flag === "file") {
-    return await stat(directory, {withFileTypes: true}, () => {
-    })
-  }
+async function getFiles(directory) {
+  return await readdir(directory, {withFileTypes: true})
 }
 
 const copyFiles = async (filesNames, directory, copyDirectory) => {
   for (const file of filesNames) {
     if (file.isFile()) {
       await copyFile(path.join(directory, file.name), path.join(copyDirectory, file.name))
-      console.log(`Файл: ${file.name} скопирован в ${copyDirectory}`);
     } else if (file.isDirectory()) {
-      copy(path.join(directoryAssets, file.name), path.join(directoryProjectDist, file.name))
+      copy(path.join(directoryAssets, file.name), path.join(copyDirectory, file.name))
     }
   }
 };
-
 
 async function createFile(path) {
   await fs.writeFile(path, "", (err) => {
   })
 }
 
-async function writeFromFile(pathFile, pathFileCopy, files, flag = "directory") {
-  if (flag === "directory") {
-    for (const file of files) {
-      const rs = fs.createReadStream(path.join(pathFile, file.name), "utf-8")
-      rs.on("data", chunk => {
-        fs.appendFile(pathFileCopy, chunk, (err) => {
-          if (err) throw err;
-        })
-      })
-    }
-  }
-  if (flag === "file") {
-    const rs = fs.createReadStream(pathFile, "utf-8")
+async function writeFromFile(pathFile, pathFileCopy, files) {
+  for (const file of files) {
+    const rs = fs.createReadStream(path.join(pathFile, file.name), "utf-8")
     rs.on("data", chunk => {
-      let data = chunk
-      data = data.replace("{{header}}", htmlData[0])
-      data = data.replace("{{articles}}", htmlData[1])
-      data = data.replace("{{footer}}", htmlData[2])
-      fs.writeFile(pathFileCopy, data, (err) => {
+      fs.appendFile(pathFileCopy, chunk, (err) => {
         if (err) throw err;
       })
     })
   }
 }
 
-async function writeFile(pathFile, pathFileCopy, flag) {
+async function writeFileHtml(templateHtml, copyTemplateHtml, tags) {
+  let file = (await fs.promises.readFile(templateHtml)).toString()
+  tags.forEach((tag) => {
+    file = file.replace(tag, components[tag])
+  })
+  await fs.writeFile(copyTemplateHtml, file, () => {
+  })
+}
+
+async function writeFile(pathFile, pathFileCopy) {
   await createFile(pathFileCopy)
-  const files = await getFiles(pathFile, flag)
-  await writeFromFile(pathFile, pathFileCopy, files, flag)
+  const files = await getFiles(pathFile)
+  await writeFromFile(pathFile, pathFileCopy, files)
 };
 
-async function writeComponents(pathFile) {
-  const rs = fs.createReadStream(pathFile, "utf-8")
-  await rs.on("data", chunk => {
-    htmlData.push(chunk.toString())
-  })
+async function writeComponents(componentsDirectory) {
+  const files = await readdir(componentsDirectory, {withFileTypes: true})
+  for (const file of files) {
+    if (file.isFile() && (path.extname(path.join(componentsDirectory, file.name)) === ".html")) {
+      const rs = await fs.promises.readFile(path.join(componentsDirectory, file.name), "utf-8")
+      let fileName = file.name.split(".")[0]
+      components[`{{${fileName}}}`] = rs.toString()
+    }
+  }
 }
 
 async function copy(directory, copyDirectory) {
   try {
     await removeDirectory(directoryProjectDist)
-    articles = writeComponents(articlesPath)
   } catch {
   }
-
   await createDirectory(copyDirectory)
+  await createDirectory(assetsDirectory)
   await copyFiles(await getFiles(directory), directory, copyDirectory)
   await writeFile(directoryStyles, cssBundle)
-  await writeFile(templateHtml, copyTemplateHtml, "file")
-
-
+  await writeFileHtml(templateHtml, copyTemplateHtml, tags)
 }
 
-copy(directoryAssets, directoryProjectDist);
+
+(async () => {
+  await writeComponents(componentsDirectory)
+  tags = Object.keys(components)
+})();
+
+copy(directoryAssets, assetsDirectory);
+
+
+
+
